7e1d774b6b619847e7788bbe05d3a0ac312c4556
Fatty <faty.aome.rojas14@gmail.com>
2023-06-16 19:54:09 -0600

    web1

---
+++ webpersonal/urls.py
@@ -0,0 +1,46 @@
+"""
+URL configuration for webpersonal project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+<<<<<<< HEAD
+from django.urls import path
+from core import views as core_views
+from portfolio import views as portfolio_views
+
+from django.conf import settings 
+
+urlpatterns = [
+    path('', core_views.home, name="home"),
+    path('about/', core_views.about, name="about"),
+    path('portfolio/', portfolio_views.portfolio, name="portfolio"),
+    path('contact/', core_views.contact, name="contact"),
+=======
+from django.urls import path, include
+from django.conf import settings 
+
+urlpatterns = [
+    path('', include ('core.urls')),
+    path('skill/', include('skill.urls')),
+    path('portfolio/', include('portfolio.urls')),
+>>>>>>> a913a34 (Ultimos cambios)
+    path('admin/', admin.site.urls),
+    
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
